b635ead07d418c1628a2022f663d046028f63234
s.islam <s.islam@bjitgroup.com>
2024-08-30 15:36:13 +0600

    updated a case with not equal operator.

--- tests/test_addition.py
+++ tests/test_addition.py
@@ -9,5 +9,5 @@ from addition import addition
 def test_addition():
     assert addition(2, 3) == 5
     assert addition(-1, 1) == 0
-    assert addition(0, 0) == 1
+    assert addition(0, 0) != 1
     assert addition(100, 200) == 300
